b9d0430ef56533bdf9ae36cb59d3caf65fe6f368
KarolinaLewinska <lewinskak95@gmail.com>
2021-10-30 20:07:24 +0200

    add menu & change nav buttons place

--- src/app/tasks-list/tasks-list.page.ts
+++ src/app/tasks-list/tasks-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuController: MenuController) { }
+  openMenu() {
+    this.menuController.enable(true, 'navMenu');
+    this.menuController.open('navMenu');
+  }
 
   ngOnInit() {
   }
