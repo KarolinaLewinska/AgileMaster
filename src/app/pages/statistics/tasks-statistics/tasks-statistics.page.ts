b2c34cfa64a13f33b41ac20f79aac27f547577e8
KarolinaLewinska <lewinskak95@gmail.com>
2021-11-15 20:36:51 +0100

    tasks statistics some methods

--- src/app/pages/statistics/tasks-statistics/tasks-statistics.page.ts
+++ src/app/pages/statistics/tasks-statistics/tasks-statistics.page.ts
@@ -11,31 +11,78 @@ export class TasksStatisticsPage implements OnInit {
   constructor(private angularFirestore: AngularFirestore) { }
 
   currentUser = firebase.auth().currentUser;
-  analystsTasksNumber: any
-  developmentTeamTasksNumber: any
+  
+  allTasksNumber: any;
+
+  analystsTasksNumber: any;
+  developmentTeamTasksNumber: any;
+  productOwnerTasksNumber: any;
+  companyTasksNumber: any;
+  educationTasksNumber: any;
+  otherTasksNumber: any;
+
+  lowPriorityTasksNumber: any;
+  mediumPriorityTasksNumber: any;
+  highPriorityTasksNumber: any;
+
+  januaryTasksNumber: any;
 
   ngOnInit() {
-    this.retrieveTasksNumberByPriority();
+    this.retrieveTasksNumberByPriority('Niski');
+    this.retrieveTasksNumberByCategory('analysts')
+    this.retrieveAllTasksNumber();
+    this.retrieveTasksNumberByMonth('-01-');
   }
 
-  // async retrieveTasksNumberByCategory(category: string) { //działa ale trzbea jakoś zmneijszyc ilosc kodu
-  //   firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(category).get()
-  //     .then((query) => {
-  //     var amountOfTasks = query.size;
-  //     this.analystsTasksNumber = amountOfTasks;
-  //     });
-  // }
+  async retrieveTasksNumberByCategory(category: string) { //działa ale trzbea jakoś zmneijszyc ilosc kodu
+    firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(category).get()
+      .then((data) => {
+      var amountOfTasks = data.size;
+      this.analystsTasksNumber = amountOfTasks;
+      });
+  }
 
-  async retrieveTasksNumberByPriority() { //działa ale trzbea jakoś zmneijszyc ilosc kodu
-    let listOfCategories: string[] = ['analysts', 'courses','education', 'otherTasks', 'developmentTeam', , 'productOwner']
-    for(var i = 0; listOfCategories.length; i++) {
-      firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(listOfCategories[i]).where('priority', '==', 'Średni').get()
-      .then((query) => {
-      var amountOfTasks = query.size;
-      let result: number  = 0;
-      result = result + amountOfTasks
-      this.analystsTasksNumber = result; //coś źle 
+  async retrieveTasksNumberByPriority(priority: string) {  //działa, ale też jakby zmniejszyć duplikacje 
+    let totalNumber = 0;
+    let listOfCategories: string[] = ['analysts','developmentTeam', 'productOwner', 'company','education', 'otherTasks'];
+    
+    for (var i = 0; listOfCategories.length; i++) {
+      firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(listOfCategories[i]).where('priority', '==', priority).get()
+      .then((data) => {
+      var numberOfTasks = data.size;
+      totalNumber += numberOfTasks;
+      this.lowPriorityTasksNumber = totalNumber; 
+      });
+    }
+  }
+
+  async retrieveAllTasksNumber() { //działa
+    let totalNumber = 0;
+    let listOfCategories: string[] = ['analysts','developmentTeam', 'productOwner', 'company','education', 'otherTasks'];
+
+    for (var i = 0; listOfCategories.length; i++) {
+      firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(listOfCategories[i]).get()
+      .then((data) => {
+      var numberOfTasks = data.size;
+      totalNumber += numberOfTasks;
+      this.allTasksNumber = totalNumber; 
+      });
+    }
+  }
+
+  async retrieveTasksNumberByMonth(month: string) {  //nie działa
+    let totalNumber = 0;
+    let listOfCategories: string[] = ['analysts','developmentTeam', 'productOwner', 'company','education', 'otherTasks'];
+    
+    for (var i = 0; listOfCategories.length; i++) {
+      firebase.firestore().collection('users').doc(this.currentUser.uid).collection('tasks').doc('category').collection(listOfCategories[i]).where('dateOfFinish', 'array-contains-any', month).get()
+      .then((data) => {
+      var numberOfTasks = data.size;
+      totalNumber += numberOfTasks;
+      this.januaryTasksNumber = totalNumber; 
       });
     }
   }
 }
+
+
